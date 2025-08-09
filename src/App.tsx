e415e16dc46e4c1d4d615831c5f8d6cd9ca8d3b6
kunwarjeepatak <kunwarjee.pathak@gmail.com>
2025-08-09 21:54:32 +0530

    react app

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import Header   from './components/Header';
 import Sidebar  from './components/Sidebar';
 import QAList   from './components/QAList';
 
-export type Category = 'all' | 'java' | 'springBoot' | 'cloud' | 'react' | 'angular';
+export type Category = 'all' | 'java' | 'springBoot' | 'aws' | 'microservices' | 'react' | 'angular' | 'Kafka' | 'Management';
 
 const categories = [
   { key: 'all' as Category,        label: 'All' },
   { key: 'java' as Category,       label: 'JAVA' },
   { key: 'springBoot' as Category, label: 'Spring' },
-  { key: 'cloud' as Category,      label: 'Cloud' },
+  { key: 'aws' as Category,      label: 'AWS' },
+  { key: 'microservices' as Category,      label: 'Microservice Architecture' },
   { key: 'react' as Category,      label: 'React' },
   { key: 'angular' as Category,    label: 'Angular' },
+  { key: 'kafka' as Category,    label: 'Kafka' },
+  { key: 'management' as Category,    label: 'Management' }
 ];
 
 function App() {
